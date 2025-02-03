d0021ee6ab7efb7e4ee2d97461125881cbc1f65c
eamon9 <eamon9@icloud.com>
2025-02-03 13:59:45 +0200

    v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 import { initFormValidation } from "./formValidation.js";
 
+function loadHeaderAndFooter() {
+  fetch("../components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+      adjustPaths(); // Izsauc pēc header ielādes
+    });
+
+  fetch("../components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+      adjustPaths(); // Izsauc pēc footer ielādes
+    });
+}
+
+document.addEventListener("DOMContentLoaded", loadHeaderAndFooter);
+
 /* function adjustPaths() {
   const currentPath = window.location.pathname;
 
