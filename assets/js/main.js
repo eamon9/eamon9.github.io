8206d2d4c7e68c1a3b681a6a05980692533f3386
eamon9 <eamon9@icloud.com>
2025-02-07 12:26:05 +0200

    add my signature =D in footer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,3 +155,12 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
   }
 });
+
+window.onload = function () {
+  if (window.location.hash) {
+    const target = document.querySelector(window.location.hash);
+    if (target) {
+      target.scrollIntoView({behavior: "smooth"});
+    }
+  }
+};
